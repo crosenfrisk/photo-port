a97a8d3c4803f3c0b6c312b893cfaaf7370d7b8c
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-04-18 17:01:36 -0500

    added __tests__ and index.test.js for Modal component; all tests pass

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -1,13 +1,14 @@
+// __tests__/Gallery.test.js
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Gallery from '..'
-
 const portrait = { name: "portraits", description: "Portraits of people in my life" };
+
 
 afterEach(cleanup)
 
-describe('Gallery is rendering', () => {
+describe('Gallery component', () => {
 
   it('renders', () => {
     render(<Gallery currentCategory={portrait} />);
@@ -16,11 +17,10 @@ describe('Gallery is rendering', () => {
   it('matches snapshot', () => {
     const { asFragment } = render(<Gallery currentCategory={portrait} />)
     expect(asFragment()).toMatchSnapshot()
-  });
+  })
+});
 
-  it('renders title', () => {
-    const { getByTestId } = render(<Gallery currentCategory={portrait} />)
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    expect( getByTestId('h1tag')).toHaveTextContent('Portraits')
-  });
+it('displays text', () => {
+  const { getByTestId } = render(<Gallery currentCategory={portrait} />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
 })
